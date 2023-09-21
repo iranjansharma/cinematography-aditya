d937df31ade5405c5f78d38e844cfc72ccc88851
Ranjan Sharma <73110351+sharma3ranjan@users.noreply.github.com>
2023-09-21 19:26:26 +0530

    About Update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,23 +1,32 @@
 const About = () => {
   return (
     <>
-      <div className='bg-about h-[500px] bg-cover bg-opacity-20 flex justify-center items-center'>
-        <p className='text-orange-500 text-5xl uppercase font-bold text-center'>
-          About Section
-        </p>
-      </div>
-      <div className='container mx-auto mt-4'>
-        <p className='w-4/6 mx-auto leading-6 text-gray-600 italic font-light '>
-          Aditya Singh is a photographer and filmmaker.Conceived in Delhi,
-          India. At the age of 20, Aditya Singh became interested in photography
-          after seeing films. Aditya was able to understand the wider picture of
-          a global company because to his upbringing during the social media and
-          digital technology boom. He began working on projects as a freelancer
-          after completing a diploma in photography, and he immediately felt the
-          transfer to filmmaking. A few years later, I began routinely working
-          for publications and customers. Currently residing in Delhi and
-          frequently traveling for business, working around the nation.
-        </p>
+      <p className='container mx-auto px-4 lg:py-6 my-6 text-black lg:text-5xl uppercase font-bold'>
+        About Section
+      </p>
+      <div className='container mx-auto flex w-full flex-wrap'>
+        <div className=''>
+          <img
+            src='/about.jpg'
+            alt='About Img'
+            className='w-[700px] lg:rounded-lg'
+          />
+        </div>
+        <div className='w-[800px] px-8'>
+          <p className='text-[14px] py-4 text-gray-600 italic font-light leading-6 lg:text-[16px]'>
+            Aditya Singh is a photographer and filmmaker.Conceived in Delhi,
+            India. At the age of 20, Aditya Singh became interested in
+            photography after seeing films. Aditya was able to understand the
+            wider picture of a global company because to his upbringing during
+            the social media and digital tecshnology boom. He began working on
+            projects as a freelancer after completing a diploma in photography,
+            and he immediately felt the transfer to filmmaking. A few years
+            later, He began routinely Working for publications and customers.
+            Currently residing in Delhi and frequently traveling for business,
+            working around the nation.
+          </p>
+          <h1 className='text-xl font-semibold'>Social Media</h1>
+        </div>
       </div>
     </>
   );
