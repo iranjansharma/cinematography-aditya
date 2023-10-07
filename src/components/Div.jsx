85d84b78165bc7b34a9391dfa875e6a3b05e389c
Ranjan Sharma <just.think2019@gmail.com>
2023-10-07 10:23:51 +0530

    Some Changes

--- src/components/Div.jsx
+++ src/components/Div.jsx
@@ -1,8 +1,5 @@
+const Div = ({ children }) => {
+  return <div className='container mx-auto px-4 py-4'>{children}</div>;
+};
 
-const Div = ({children}) => {
-  return (
-    <div className="container mx-auto px-4">{children}</div>
-  )
-}
-
-export default Div
+export default Div;
--- src/components/ImageModal.jsx
+++ src/components/ImageModal.jsx
@@ -1,23 +1,100 @@
 const ImageModal = () => {
-    return (
+  return (
+    <div className='grid grid-cols-1 md:grid-cols-4 gap-4'>
+      <div className='grid gap-4'>
         <div>
-            <button
-                className='btn'
-                onClick={() => document.getElementById('my_modal_2').showModal()}
-            >
-                open modal
-            </button>
-            <dialog id='my_modal_2' className='modal'>
-                <div className='modal-box'>
-                    <h3 className='font-bold text-lg'>Hello!</h3>
-                    <p className='py-4'>Press ESC key or click outside to close</p>
-                </div>
-                <form method='dialog' className='modal-backdrop'>
-                    <button>close</button>
-                </form>
-            </dialog>
+          <img
+            className='h-auto max-w-full rounded-lg'
+            src='https://github.com/iranjansharma/cinematography-aditya/blob/main/src/assets/Fashion/0B1A1087.jpg?raw=true'
+            alt=''
+          />
         </div>
-    );
+        <div>
+          <img
+            className='h-auto max-w-full rounded-lg'
+            src='https://github.com/iranjansharma/cinematography-aditya/blob/main/src/assets/Fashion/410A0893.jpg?raw=true'
+            alt=''
+          />
+        </div>
+        <div>
+          <img
+            className='h-auto max-w-full rounded-lg'
+            src='https://github.com/iranjansharma/cinematography-aditya/blob/main/src/assets/Fashion/410A0977-V.jpg?raw=true'
+            alt=''
+          />
+        </div>
+      </div>
+      <div className='grid gap-4'>
+        <div>
+          <img
+            className='h-auto max-w-full rounded-lg'
+            src='https://github.com/iranjansharma/cinematography-aditya/blob/main/src/assets/Fashion/410A1170.jpg?raw=true'
+            alt=''
+          />
+        </div>
+        <div>
+          <img
+            className='h-auto max-w-full rounded-lg'
+            src='https://github.com/iranjansharma/cinematography-aditya/blob/main/src/assets/Fashion/410A1432-V.jpg?raw=true'
+            alt=''
+          />
+        </div>
+        <div>
+          <img
+            className='h-auto max-w-full rounded-lg'
+            src='https://github.com/iranjansharma/cinematography-aditya/blob/main/src/assets/Fashion/410A1526%202.jpg?raw=true'
+            alt=''
+          />
+        </div>
+      </div>
+      <div className='grid gap-4'>
+        <div>
+          <img
+            className='h-auto max-w-full rounded-lg'
+            src='https://github.com/iranjansharma/cinematography-aditya/blob/main/src/assets/Fashion/410A1722-V.jpg?raw=true'
+            alt=''
+          />
+        </div>
+        <div>
+          <img
+            className='h-auto max-w-full rounded-lg'
+            src='https://github.com/iranjansharma/cinematography-aditya/blob/main/src/assets/Fashion/410A1722-V.jpg?raw=true'
+            alt=''
+          />
+        </div>
+        <div>
+          <img
+            className='h-auto max-w-full rounded-lg'
+            src='https://github.com/iranjansharma/cinematography-aditya/blob/main/src/assets/Fashion/0B1A1087.jpg?raw=true'
+            alt=''
+          />
+        </div>
+      </div>
+      <div className='grid gap-4'>
+        <div>
+          <img
+            className='h-auto max-w-full rounded-lg'
+            src=' https://github.com/iranjansharma/cinematography-aditya/blob/main/src/assets/Fashion/410A1170.jpg?raw=true'
+            alt=''
+          />
+        </div>
+        <div>
+          <img
+            className='h-auto max-w-full rounded-lg'
+            src='https://github.com/iranjansharma/cinematography-aditya/blob/main/src/assets/Fashion/410A0893.jpg?raw=true'
+            alt=''
+          />
+        </div>
+        <div>
+          <img
+            className='h-auto max-w-full rounded-lg'
+            src='https://github.com/iranjansharma/cinematography-aditya/blob/main/src/assets/Fashion/410A1722-V.jpg?raw=true'
+            alt=''
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ImageModal;
